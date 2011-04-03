bd7220917cc707e3de1e560f887fd4942db30de1
Raptus <dev@raptus.com>
2011-04-03 10:46:59 +0000

    first version is ready for testing

--- raptus/article/mapsjunaio/extender.py
+++ raptus/article/mapsjunaio/extender.py
@@ -5,31 +5,87 @@ from archetypes.schemaextender.interfaces import ISchemaExtender
 from archetypes.schemaextender.field import ExtensionField
 
 from Products.Archetypes import atapi
+from Products.validation import V_REQUIRED
+from Products.ATContentTypes.configuration import zconf
+
+try: # blob
+    from plone.app.blob.field import ImageField
+except:
+    from Products.Archetypes.Field import ImageField
+
+from raptus.article.maps.content.map import Map
+from raptus.article.maps.content.marker import Marker
+from raptus.article.core import RaptusArticleMessageFactory as _
+
+
 
 class StringField(ExtensionField, atapi.StringField):
     """ StringField
     """
+class ImageField(ExtensionField, ImageField):
+    """ ImageField
+    """
+
 
 
 class MapsExtender(object):
     """ add junaio link in maps edit view
     """
     implements(ISchemaExtender)
-    adapts(IArticle)
+    adapts(Map)
 
-    fields = [        
+    fields = [
         StringField('junaiolink',
             required = False,
-            searchable = False,
             widget = atapi.StringWidget(
                 description = '',
-                label = _at(u'label_junaio_label', default=u'Link for Junaio channel'),
+                macro = 'widget_junaio_link',
+                label = _(u'label_junaio_label', default=u'Link for Junaio channel'),
             )
         ),
     ]
 
     def __init__(self, context):
-         self.context = context
+        self.context = context
+         
+    def getFields(self):
+        return self.fields
+
+class MarkerExtender(object):
+    """ add junaio link in maps edit view
+    """
+    implements(ISchemaExtender)
+    adapts(Marker)
+
+    fields = [
+        ImageField('image',
+            required=False,
+            languageIndependent=True,
+            storage = atapi.AnnotationStorage(),
+            swallowResizeExceptions = zconf.swallowImageResizeExceptions.enable,
+            pil_quality = zconf.pil_config.quality,
+            pil_resize_algo = zconf.pil_config.resize_algo,
+            max_size = zconf.ATImage.max_image_dimension,
+            sizes= {'large'   : (768, 768),
+                    'preview' : (400, 400),
+                    'mini'    : (200, 200),
+                    'thumb'   : (128, 128),
+                    'tile'    :  (64, 64),
+                    'icon'    :  (32, 32),
+                    'listing' :  (16, 16),
+                   },
+            validators = (('isNonEmptyFile', V_REQUIRED),
+                          ('checkImageMaxSize', V_REQUIRED)),
+            widget = atapi.ImageWidget(
+                description = '',
+                label= _(u'label_image', default=u'Image to display on junaio'),
+                show_content_type = False,
+            )
+        ),
+        ]
+
+    def __init__(self, context):
+        self.context = context
          
     def getFields(self):
         return self.fields
--- raptus/article/mapsjunaio/xml/junaio.py
+++ raptus/article/mapsjunaio/xml/junaio.py
@@ -1,5 +1,10 @@
+from zope import component
+
+from Acquisition import aq_inner
+
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
 
 from raptus.article.maps.interfaces import IMarkers
 
@@ -7,6 +12,55 @@ class View(BrowserView):
     
     __call__ = ViewPageTemplateFile('junaio.pt')
     
+    def __init__(self, context, request):
+        super(View,self).__init__(context, request)
+        self.props = getToolByName(self.context, 'portal_properties').raptus_article
     
-    def makers(self):
-        return IMarkers(self.context)
+    def markers(self):
+        context = aq_inner(self.context)
+        markers = list()
+        for brain in IMarkers(context).getMarkers():
+            object = brain.getObject()
+            scales = component.queryMultiAdapter((object, object.REQUEST), name='images')
+            di = dict(brain = brain,
+                      obj = object,
+                      uid = brain.UID,
+                      title = object.Title(),
+                      text = object.getText(),
+                      latitude = object.getLatitude(),
+                      longitude = object.getLongitude(),
+                      icon = self._thumbnail(object, scales),
+                      thumbnail = self._icon(object, scales)
+                      )
+            markers.append(di)
+        return markers
+            
+    def _thumbnail(self, object, scales):
+        thumb = None
+        thumbnail_w = self.props.getProperty('junaio_thumbnail_width')
+        thumbnail_h = self.props.getProperty('junaio_thumbnail_height')
+        if scales.field('image'):
+            thumb = scales.scale('image', width=thumbnail_w, height=thumbnail_h)
+        if thumb is not None:
+            return thumb.absolute_url()
+        else:
+            return self._default(self.props.getProperty('junaio_thumbnail_default'))
+        
+    def _icon(self, object, scales):
+        icon = None
+        icon_w = self.props.getProperty('junaio_icon_width')
+        icon_h = self.props.getProperty('junaio_icon_height')
+        if scales.field('image'):
+            icon = scales.scale('image', width=icon_w, height=icon_h)
+        if icon is not None:
+            return icon.absolute_url()
+        else:
+            return self._default(self.props.getProperty('junaio_icon_default'))
+    
+    def _default(self, name):
+        portal_state = component.getMultiAdapter((self.context, self.request), name=u'plone_portal_state')
+        site = portal_state.portal()
+        return '%s/%s' %(site.absolute_url(), name)
+    
+    
+ 
